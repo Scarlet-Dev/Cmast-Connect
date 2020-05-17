f164d87eccb28e6415f414bfd7f9af5e29beae0e
Scarlet <developerscarlet@gmail.com>
2020-05-17 17:18:00 -0400

    Added reference to DataAccess Lib.

--- cmast-cms/CMASTConnect.DataAccess/CMASTConnectContext.cs
+++ cmast-cms/CMASTConnect.DataAccess/CMASTConnectContext.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using CMASTConnect.DTO.Models;
 
 namespace CMASTConnect.DataAccess
 {
     class CMASTConnectContext : DbContext
     {
-        public CMASTConnectContext(DbContextOptions<CMASTConnectContext> options) :
-            base(options)
-        {
+        private readonly ILoggerFactory _logger;
 
+        public CMASTConnectContext(DbContextOptions<CMASTConnectContext> options) :
+            base(options) { }
+
+        protected CMASTConnectContext()
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLoggerFactory(_logger);
+                optionsBuilder.UseMySQL("");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("CMAST_CONNECT");
+            modelBuilder.Entity<User>();
+            modelBuilder.Entity<Post>();
         }
     }
 }
