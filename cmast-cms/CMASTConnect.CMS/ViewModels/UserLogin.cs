89c8fc1d2b94445e9debf137cc2d5f897e4e7e73
Scarlet <developerscarlet@gmail.com>
2020-05-18 20:37:08 -0400

    Added UserLogin model.

---
+++ cmast-cms/CMASTConnect.CMS/ViewModels/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using CMASTConnect.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMASTConnect.CMS.ViewModels
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public List<UserClaims> UserClaim { get; set; }
+    }
+}
