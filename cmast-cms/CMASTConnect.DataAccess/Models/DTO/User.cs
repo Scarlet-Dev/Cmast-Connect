f06a864c501aa026549feee9b1a2c39de9923d06
Scarlet <developerscarlet@gmail.com>
2020-05-18 20:32:04 -0400

    Added model properties to User Model.

--- cmast-cms/CMASTConnect.DataAccess/Models/DTO/User.cs
+++ cmast-cms/CMASTConnect.DataAccess/Models/DTO/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,25 @@ using CMASTConnect.DTO;
 
 namespace CMASTConnect.DTO.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class User
     {
+        [Required, Key]
+        public int Id { get; set; }
+
+        [Required]
         public string Username { get; }
-        public string Password { get; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set;  }
+
         public List<UserClaims> Claims { get; }
+
+        public DateTime ExpirationDate { get; set; }
     }
 }
