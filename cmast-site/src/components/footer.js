ded4ceadec3201cd2eb61b996550fbe43cd46885
Scarlet <developerscarlet@gmail.com>
2020-05-19 20:34:37 -0400

    Added footer component.

---
+++ cmast-site/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>
+        <div>
+            <h3>Made by Scarlet Developer</h3>
+            <sub>Copyright 2020</sub>
+            <sub>Link to Github</sub>
+        </div>
+    </footer>
+)
+
+export default Footer
