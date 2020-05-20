c6c5446162e21ec2163221947a9e0fd291fd3a69
Scarlet <developerscarlet@gmail.com>
2020-05-19 20:35:45 -0400

    Added activities page.

---
+++ cmast-site/src/pages/activities.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from "gatsby";
+
+import Layout from "../components/layout"
+import SEO from "../components/seo";
+
+const ActivitiesPage = () => (
+    <Layout>
+        <SEO title="Activities" />
+        <h1>Activities</h1>
+    </Layout>
+)
+
+export default ActivitiesPage
