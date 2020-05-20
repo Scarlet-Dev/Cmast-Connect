975ca216b925055a36623009781d3014d6794dd9
Scarlet <developerscarlet@gmail.com>
2020-05-19 20:35:32 -0400

    Added contact page.

---
+++ cmast-site/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const ContactsPage = () => (
+    <Layout>
+        <SEO title="" />
+        <h2>Contact Us!</h2>
+    </Layout>
+)
+
+export default ContactsPage
