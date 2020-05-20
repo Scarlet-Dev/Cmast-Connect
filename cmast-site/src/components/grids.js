6d7e4edb6cb30517dc9e1880cdceab7e6a98520c
Scarlet <developerscarlet@gmail.com>
2020-05-19 20:34:51 -0400

    Added grid component.

---
+++ cmast-site/src/components/grids.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BasicGrid = () => (
+    <div>
+        <div>
+            <p>Items Here.</p>
+        </div>
+    </div>
+)
+
+export const AdvancedGrid = () => (
+    <div>
+        <div>
+            <p>More advanced exports.</p>
+        </div>
+    </div>
+)
+
+export default BasicGrid
