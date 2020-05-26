2646edc3a15f3823b98042b1c656563a4f3b9177
Scarlet <developerscarlet@gmail.com>
2020-05-25 21:57:42 -0400

    Removed Mongo attributes from Form Template.

--- cmast-cms/CMASTConnect.DataAccess/Models/DTO/FormTemplate.cs
+++ cmast-cms/CMASTConnect.DataAccess/Models/DTO/FormTemplate.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
@@ -11,28 +11,19 @@ namespace CMASTConnect.DataAccess.Models.DTO
     /// </summary>
     public class FormTemplate
     {
-        [BsonId, BsonRequired]
-        [BsonRepresentation(BsonType.ObjectId)]
+        
         public int Id { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string FormName { get; set; }
 
-        [BsonRepresentation(BsonType.Array)]
         public string[] FormData { get; set; }
 
-        [BsonRepresentation(BsonType.DateTime)]
         public DateTime CreatedOn { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
         public string CreatedBy { get; set; }
         
-        [BsonRepresentation(BsonType.DateTime)]
         public DateTime LastUpdatedOn { get; set; }
 
-        [BsonDateTimeOptions(DateOnly = false, 
-            Kind =DateTimeKind.Utc, 
-            Representation = BsonType.DateTime)]
         public string LastUpdatedBy { get; set; }
     }
 }
