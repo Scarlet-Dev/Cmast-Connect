618d45b2ed43ef1b9137d6497eec8f4a67066adf
Scarlet <developerscarlet@gmail.com>
2020-05-30 18:14:15 -0400

    Added IQueryRepoBase interface.

---
+++ cmast-cms/CMASTConnect.DataAccess/Interfaces/IQueryRepoBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMASTConnect.DataAccess.Interfaces
+{
+    interface IQueryRepoBase<Q>
+    {
+        IList<Q> Filter();
+
+        IList<Q> Query();
+    }
+}
