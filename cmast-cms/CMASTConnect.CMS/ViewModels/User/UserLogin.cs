28807554f136bc33f528552c747002311801cbce
Scarlet <developerscarlet@gmail.com>
2020-05-30 19:18:55 -0400

    Added UserLogin ViewModel.

---
+++ cmast-cms/CMASTConnect.CMS/ViewModels/User/UserLogin.cs
@@ -0,0 +1,11 @@
+﻿namespace CMASTConnect.CMS.ViewModels
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool StayLoggedIn { get; set; }
+    }
+}
