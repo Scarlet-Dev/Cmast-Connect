8211675b01c96706fbfb37c7ae1b6649278a8e2e
Scarlet <developerscarlet@gmail.com>
2020-05-30 18:51:57 -0400

    Added IFormRepository interface.

---
+++ cmast-cms/CMASTConnect.DataAccess/Interfaces/IRepositories/IFormRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CMASTConnect.DataAccess.Interfaces.IRepositories
+{
+    interface IFormRepository<F>: IRepositoryBase<F>, IQueryRepoBase<F>
+    {
+        string PublishForm(F model);
+
+        string UnPublishForm(F model);
+    }
+}
