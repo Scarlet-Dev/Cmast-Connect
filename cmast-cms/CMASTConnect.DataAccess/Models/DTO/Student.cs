4e635dd72dd645669d2eef3bc01a93c15a57f746
Scarlet <developerscarlet@gmail.com>
2020-05-30 19:23:15 -0400

    Removed DataAnnotations imports and attributes.

--- cmast-cms/CMASTConnect.DataAccess/Models/DTO/Student.cs
+++ cmast-cms/CMASTConnect.DataAccess/Models/DTO/Student.cs
@@ -1,40 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CMASTConnect.DataAccess.Models.DTO
 {
     public class Student
     {
-        [Required, Key]
         public string Id { get; set; }
 
         public string Name { get; set; }
 
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required, ForeignKey("")]
         public int ProgramId { get; set; }
 
-        [DataType(DataType.Date)]
         public string StudentType { get; set; }
 
-        [DataType(DataType.Text)]
         public string s { get; set; }
 
-        [DataType(DataType.Text)]
         public string CurrentProgramEnrolled { get; set; }
 
-        [DataType(DataType.Text)]
         public string EnrollmentType { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime EnrollmentYear { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
     }
 }
