d808b4e79783ff699c766d3e4d81f41e9d7d1f99
Scarlet <developerscarlet@gmail.com>
2020-05-30 18:24:52 -0400

    Added IServiceBae interface.

---
+++ cmast-cms/CMASTConnect.DataAccess/Interfaces/IServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMASTConnect.DataAccess.Interfaces
+{
+    interface IServiceBase<S> : IDisposable
+    {
+    }
+}
