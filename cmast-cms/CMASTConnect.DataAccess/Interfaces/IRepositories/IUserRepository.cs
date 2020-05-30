d2fe2a5919f52babde6601122dc70fdc0536cc8c
Scarlet <developerscarlet@gmail.com>
2020-05-30 19:02:26 -0400

    Implemented IRepoBase; Also Added UpdateProfile and ResetPassword method.

--- cmast-cms/CMASTConnect.DataAccess/Interfaces/IRepositories/IUserRepository.cs
+++ cmast-cms/CMASTConnect.DataAccess/Interfaces/IRepositories/IUserRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using CMASTConnect.DataAccess.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CMASTConnect.DataAccess.Interfaces
 {
-    interface IUserRepository
+    interface IUserRepository<U> : IRepositoryBase<U>, IQueryRepoBase<U>
     {
+        string ResetPassword(U model);
+
+        string UpdateProfile(U model);
     }
 }
