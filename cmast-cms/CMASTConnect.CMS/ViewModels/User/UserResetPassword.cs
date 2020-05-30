c2a0ae01aa643585629b00fa39ea8c372a185043
Scarlet <developerscarlet@gmail.com>
2020-05-30 19:21:22 -0400

    Added UserResetPassword ViewModel.

---
+++ cmast-cms/CMASTConnect.CMS/ViewModels/User/UserResetPassword.cs
@@ -0,0 +1,11 @@
+﻿namespace CMASTConnect.CMS.ViewModels
+{
+    public class UserResetPassword
+    {
+        public string Username { get; }
+
+        public string ResetPassword { get; }
+
+        public string ConfirmResetPassword { get; }
+    }
+}
