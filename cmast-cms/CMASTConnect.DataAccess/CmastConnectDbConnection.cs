599260d172b02a5a05a857a7b7aae8e986da10bf
Scarlet <developerscarlet@gmail.com>
2020-05-30 18:19:32 -0400

    Removed CMASTConnectContext to CmastConnectDbConnection.

---
+++ cmast-cms/CMASTConnect.DataAccess/CmastConnectDbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CMASTConnect.DTO.Models;
+using MySqlConnector;
+using MongoDB.Driver;
+
+namespace CMASTConnect.DataAccess
+{
+    class CmastConnectDbConnection
+    {
+        private CmastConnectDbConnection()
+        {
+
+        }
+
+        private string _DatabaseName = string.Empty;
+
+        
+        
+    }
+}
