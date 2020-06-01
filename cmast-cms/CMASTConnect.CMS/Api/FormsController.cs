1c01b876ab2b1975f3ed760016f426c8cb9b8c89
Scarlet <developerscarlet@gmail.com>
2020-06-01 00:53:48 -0400

    Added FormsController.

---
+++ cmast-cms/CMASTConnect.CMS/Api/FormsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMASTConnect.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMASTConnect.CMS.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormsController : ControllerBase
+    {
+        [HttpGet]
+        public Post Get(int formId)
+        {
+            return new Post();
+        }
+
+        [HttpPost("/saveFormData")]
+        public ActionResult ActionResult(Form post)
+        {
+            return Ok();
+        }
+    }
+}
