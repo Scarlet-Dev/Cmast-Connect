cc5d72949a201dd29c8e8f991afa528042b894c1
Scarlet <developerscarlet@gmail.com>
2020-06-01 00:05:25 -0400

    Added PostEdit model.

---
+++ cmast-cms/CMASTConnect.CMS/ViewModels/Post/PostEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMASTConnect.Models.DTO;
+
+namespace CMASTConnect.CMS.ViewModels.PostVM
+{
+    public class PostEdit : Post
+    {
+        public PostEdit() : base()
+        {
+
+        }
+    }
+}
