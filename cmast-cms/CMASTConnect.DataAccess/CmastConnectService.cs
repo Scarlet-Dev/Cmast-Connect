035a26480a5f8f94bc55691ff8328217db335d69
Scarlet <developerscarlet@gmail.com>
2020-05-31 22:59:37 -0400

    Added CmastConnectService.

---
+++ cmast-cms/CMASTConnect.DataAccess/CmastConnectService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMASTConnect.DataAccess
+{
+    public class CmastConnectService
+    {
+        public CmastConnectService()
+        {
+
+        }
+    }
+}
