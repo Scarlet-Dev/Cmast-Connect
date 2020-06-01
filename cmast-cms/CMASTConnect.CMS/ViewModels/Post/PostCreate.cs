471bee248fc26f6fc854c7f178b382e450ea492a
Scarlet <developerscarlet@gmail.com>
2020-06-01 00:07:55 -0400

    Added PostCreate ViewModel.

---
+++ cmast-cms/CMASTConnect.CMS/ViewModels/Post/PostCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMASTConnect.Models.DTO;
+
+namespace CMASTConnect.CMS.ViewModels.PostVM
+{
+    public class PostCreate : Post
+    {
+        public PostCreate () : base()
+        {
+
+        }
+    }
+}
