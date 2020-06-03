11f665510a9fec8c81e136dfd4097668ea0685bd
Scarlet <developerscarlet@gmail.com>
2020-06-02 23:31:04 -0400

    Added React, Gatsby, Components imports.

---
+++ cmast-site/src/pages/blog.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Card from "../components/card"
+import { Link } from "gatsby";
+
+const BlogCard = () => (
+    <Card>
+
+    </Card>
+)
+
+const BlogList = () => (
+    <></>
+)
+
+const BlogPage = () => (
+    <Layout>
+        <SEO title="Blog"/>
+        <h1></h1>
+        <BlogList></BlogList>
+    </Layout>
+)
