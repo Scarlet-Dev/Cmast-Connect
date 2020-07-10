237f07b1e1bccfdd2affe3436168c222b799dfad
Scarlet <developerscarlet@gmail.com>
2020-07-10 18:25:46 -0400

    Extend Post base model to PostSearch model

--- cmast-cms/CMASTConnect.Models/Models/Search/PostSearch.cs
+++ cmast-cms/CMASTConnect.Models/Models/Search/PostSearch.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using CMASTConnect.Models.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CMASTConnect.Models.Search
 {
-    public class PostSearch
+    public class PostSearch : Post
     {
-        public string Title { get; set; }
+        
 
-        public string[] Category { get; set; }
+        public PostSearch(int id, string title, string[] category, string author,
+            DateTime publishedDate, string updatedBy, DateTime updatedOn) : 
+            base(id, title, category, author,publishedDate, updatedBy, updatedOn)
+        {
 
-        public string Author { get; set; }
-
-        public DateTime PublishedDate { get; set; }
+        }
     }
 }
