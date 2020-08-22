b7c80a583f11e5af0bb3a18a5f77333821638259
Scarlet <developerscarlet@gmail.com>
2020-08-21 20:13:49 -0400

    Fix base decaration for PostSearch

--- cmast-cms/CMASTConnect.Models/Models/Search/PostSearch.cs
+++ cmast-cms/CMASTConnect.Models/Models/Search/PostSearch.cs
@@ -7,13 +7,29 @@ namespace CMASTConnect.Models.Search
 {
     public class PostSearch : Post
     {
-        
+        //public PostSearch(int id, string title, string[] category, string author,
+        //    DateTime publishedDate, string updatedBy, DateTime updatedOn)
+        //{
+        //    Id = id;
+        //    Title = title;
+        //    Category = category;
+        //    Author = author;
+        //    PublishedDate = publishedDate;
+        //    UpdatedBy = updatedBy;
+        //    UpdatedOn = updatedOn;
+        //}
 
-        public PostSearch(int id, string title, string[] category, string author,
-            DateTime publishedDate, string updatedBy, DateTime updatedOn) : 
-            base(id, title, category, author,publishedDate, updatedBy, updatedOn)
+        public PostSearch(int id, string title, string[] category, string author, bool isPublished, DateTime publishedDate, 
+            string updatedBy, DateTime updatedOn) : base(id, title, category, author, isPublished, publishedDate, updatedBy, updatedOn)
         {
-
         }
+
+        public new int Id { get; }
+        public new string Title { get; }
+        public new string[] Category { get; }
+        public new string Author { get; }
+        public new DateTime PublishedDate { get; }
+        public new string UpdatedBy { get; }
+        public new DateTime UpdatedOn { get; }
     }
 }
